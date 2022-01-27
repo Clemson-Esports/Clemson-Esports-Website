fe253f47124d0aac84b09d70c3f8a7a026c244af
Charles Truluck <ctruluc@clemson.edu>
2022-01-27 12:51:37 -0500

    updated roster styling, added tech team as a roster, updated Twitch channel.

--- roster_machine.js
+++ roster_machine.js
@@ -5,8 +5,6 @@ const { Client, Intents, Interaction } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 const trackedRoles = require('./roster_roles.json').roles;
 
-console.log("hit");
-
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	let roster = {}
@@ -14,25 +12,26 @@ client.once('ready', () => {
 	const guild = client.guilds.cache.get("215845807801237514");
 	guild.roles.fetch().then((roles) => {
 		roles.forEach(Role => {
-			
-			guild.members.fetch().then((members) => {
-				let Members = members.filter(member => member.roles.cache.find(role => role == Role)).map(member => { 
-					return { 
-						"name": member.nickname || member.user.username, 
-						"image_cdn": member.user.avatarURL() ?? ""
-					} 
+			if (Role.name != "Tech Team Leader") { // Leader is in tech team
+				guild.members.fetch().then((members) => {
+					let Members = members.filter(member => member.roles.cache.find(role => role == Role)).map(member => { 
+						return { 
+							"name": member.nickname || member.user.username, 
+							"image_cdn": member.user.avatarURL() ?? ""
+						} 
+					});
+					Members = Members.filter(member => member !== null);
+					if (trackedRoles.some( roleName => Role.name.includes(roleName)) && Members.length != 0) {
+						console.log(Role.name)
+						roster[Role.name] = {
+							"members": Members,
+							"color": Role.hexColor,
+							"icon": Role.iconURL() ?? ""
+						};
+						global.roster = roster;
+					}
 				});
-				Members = Members.filter(member => member !== null);
-				if (trackedRoles.some( roleName => Role.name.includes(roleName)) && Members.length != 0) {
-					console.log(Role.name)
-					roster[Role.name] = {
-						"members": Members,
-						"color": Role.hexColor,
-						"icon": Role.iconURL() ?? ""
-					};
-					global.roster = roster;
-				}
-			});
+			}
 		});
 	});
 });
